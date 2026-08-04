08322fd1fa08fc59d5082958418bccae996b55a6
Dvodnenko <thekovalskii@gmail.com>
2026-08-04 18:41:07 +0300

    implement action selection

--- src/agent.py
+++ src/agent.py
@@ -1,3 +1,4 @@
+import random
 from dataclasses import dataclass
 
 import numpy as np
@@ -14,3 +15,8 @@ class Agent:
     @property
     def pref_length(self):
         return np.sqrt((self.preferences*self.preferences).sum())
+
+    def select_track(self):
+        if random.uniform(0, 1) <= self.epsilon:
+            return songs_db[random.randint(0, len(songs_db)-1)] # random track
+        return songs_db[np.argmax(quality_matrix.dot(self.preferences))]
