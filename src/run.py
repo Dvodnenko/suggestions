2a44c6dbdd2caeec496fe547b52a866a4dcccc4b
Dvodnenko <thekovalskii@gmail.com>
2026-08-08 21:27:32 +0300

    test database work

--- src/run.py
+++ src/run.py
@@ -1,10 +1,20 @@
 import numpy as np
 
 from .agent import Agent
+from .db import Database
+from .sr import SongRecord
+from .utils import build_embedding
 
 
-init_preferences = np.zeros(5) + 1/np.sqrt(5)
-agent = Agent(init_preferences, 0.1, 0.8)
+db = Database()
+db.create_collection()
 
-while True:
-    agent.step()
+song1 = SongRecord(
+    1, "Bankroll", "Lil Uzi Vert", 
+    build_embedding(0.6, 0.8, 0.7), 0.6, 0.8, 0.7)
+song2 = SongRecord(
+    2, "Love Thy Enemies", "Future", 
+    build_embedding(0.3, 0.45, 0.2), 0.3, 0.45, 0.2)
+
+db.insert_songs([song1, song2])
+print(db.search([0.3, 0.45, 0.2], top_k=1))
